14318e4654390f573364e37cfd7ed52be464fff0
Nulldata <thebreadcat@gmail.com>
2015-05-25 13:26:42 +0200

    Implemented Worker Task management

--- src/runtime.rs
+++ src/runtime.rs
@@ -1,4 +1,4 @@
-use std::sync::mpsc::{Receiver, Sender, channel};
+use std::sync::mpsc::{Receiver, Sender, channel, TryRecvError};
 use std::sync::Arc;
 use std::thread;
 use ::num_cpus;
@@ -61,6 +61,12 @@ impl Task {
     Task { code : code
          , stack: Vec::new() }
   }
+
+  pub fn step( &mut self ) -> bool {
+    // TODO
+    println!("Hello world from a task!");
+    false
+  }
 }
 
 // An immutable data-structure to store all constant data needed at runtime
@@ -97,14 +103,13 @@ impl ConstantTable {
 pub struct WorkerHandle {
   outbox     : Sender<WorkerCommand>,
   task_count : u32,
+  handle     : thread::JoinHandle<()>
 }
 
 // Status reports that the worker will report back to the scheduler
 pub enum WorkerReport {
   Dead,
-  Asleep,
-  Available,
-  Okay
+  Finsihed,
 }
 
 // Commands that the scheduler can send the worker
@@ -114,27 +119,122 @@ pub enum WorkerCommand {
 }
 
 // A concurrent task manager for it's own thread
-pub struct Worker<'a> {
-  outbox       : Sender<WorkerReport>,
-  inbox        : Receiver<WorkerCommand>,
-  rewrites     : u32,
-  tasks        : Vec<Task>,
-  current_task : Option<&'a mut Task>
+pub struct Worker {
+  outbox        : Sender<WorkerReport>,
+  inbox         : Receiver<WorkerCommand>,
+  rewrites      : u32,
+  rewrite_quota : u32,
+  tasks         : Vec<Task>,
+  alive         : bool,
+  current_task  : usize
 }
 
-impl<'a> Worker<'a> {
-  fn new( inbox : Receiver<WorkerCommand>, outbox : Sender<WorkerReport> )
-     -> Worker<'a> {
+impl Worker {
+  fn new( inbox : Receiver<WorkerCommand>, outbox : Sender<WorkerReport>
+        , quota : u32 ) -> Worker {
  
-    Worker { outbox  : outbox
-           , inbox   : inbox
-           , rewrites: 0
-           , tasks   : Vec::new()
-           , current_task : None }
+    Worker { outbox       : outbox
+           , inbox        : inbox
+           , rewrites     : 0
+           , rewrite_quota: quota
+           , tasks        : Vec::new()
+           , alive        : true
+           , current_task : 0 }
   }
 
   fn run( &mut self ) {
-    // TODO
+    println!("Up and running, waiting for first task");
+    self.next_task();
+
+    while self.alive {
+      println!("Begin execution");
+      if self.step_current_task() {
+        self.rewrites += 1;
+        if self.rewrites == self.rewrite_quota {
+          self.rewrites = 0;
+
+          // Try to switch to the next task
+          if !self.next_task() {
+            // If we fail to get the next task, that's because we've been killed
+            break
+          }
+        }
+      } else {
+        self.remove_current_task();
+      }
+
+      self.check_for_interrupt();
+    }
+
+    self.shutdown();
+  }
+
+  fn next_task( &mut self ) -> bool {
+    // If there's no tasks, wait for one
+    if self.tasks.len() == 0 {
+      self.wait_for_task();
+      self.current_task = 0;
+      // Check if we've been killed while waitng for the next task
+      if !self.alive {
+        return false
+      }
+    }
+    // Get the next task in a vector ( cyclic )
+    self.current_task = ( self.current_task + 1 ) % self.tasks.len();
+    true
+  }
+
+  fn wait_for_task( &mut self ) {
+    // Unwrap because if this call fails, our scheduler has died, and 
+    // at that point, does anything really matter...?
+    match self.inbox.recv().unwrap() {
+      WorkerCommand::SpawnTask( t ) => {
+        self.tasks.push( t );
+      },
+      WorkerCommand::Die => {
+        self.alive = false; // :(
+      }
+    }
+  }
+
+  fn step_current_task( &mut self ) -> bool {
+    let task = &mut self.tasks[self.current_task];
+    task.step()
+  }
+
+  // Check if there's a command sent to the worker
+  // if there's no active tasks, we block and wait
+  fn check_for_interrupt( &mut self ) {
+    match self.inbox.try_recv() {
+      Ok( WorkerCommand::SpawnTask( t ) ) => {
+        self.tasks.push( t );
+      },
+      Ok( WorkerCommand::Die ) => {
+        self.alive = false; // :(
+      },
+      Err( TryRecvError::Disconnected ) =>
+        panic!( "Worker got disconneced from scheduler!" ),
+      // There's no new command, so ignore it
+      _ => {}
+    }
+  }
+
+  fn remove_current_task( &mut self ) {
+    // TODO: Do we actually care about task order?
+    //       If yes then we use remove() else swap_remove()
+    self.tasks.remove( self.current_task );
+
+    // Our current task index is automatically pointing to the next
+    // task after a we remove one, but we have to make sure it's in bounds:
+    self.current_task = self.current_task % self.tasks.len();
+
+    // Tell the scheduler we finished a task
+    self.outbox.send( WorkerReport::Finsihed );
+  }
+
+  fn shutdown( &mut self ) {
+    // TODO: Shutdown the task properly
+    self.outbox.send( WorkerReport::Dead );
   }
 }
 
@@ -162,13 +262,16 @@ impl Scheduler {
 
   fn spawn_worker( &mut self ) -> &mut WorkerHandle {
     let (wo, wi) = channel();
-    let mut worker = Worker::new( wi, self.outbox_seed.clone() );
+    let mut worker =
+      Worker::new( wi, self.outbox_seed.clone(), self.rewrite_quota );
 
     // Spawn the worker in it's own thread
-    thread::spawn( move || worker.run() );
+    let h = thread::spawn( move || { worker.run() } );
 
     // Create and register a handle for the worker
-    let worker_handle = WorkerHandle { task_count: 0, outbox: wo };
+    let worker_handle =
+      WorkerHandle { task_count: 0, outbox: wo, handle : h };
+
     self.worker_handles.push( worker_handle );
     
     // And return a reference to that handle
@@ -211,13 +314,19 @@ impl Scheduler {
   }
 
   fn spawn_task( &mut self, task : Task ) {
+    // We borrow self mutably here so we scope it -
     {
       let wh = self.get_available_worker();
 
       wh.outbox.send( WorkerCommand::SpawnTask( task ) );
       wh.task_count += 1;
     }
+    // - so we can mutate it afterwards
     self.task_count += 1;
+  }
+
+  fn run( &mut self ) {
+    // TODO
   }
 }
 
@@ -253,5 +362,6 @@ impl Runtime {
 
   pub fn run( &mut self ) {
     self.scheduler.spawn_task( Task::new( self.constants.main() ) );
+    self.scheduler.run();
   }
 }
